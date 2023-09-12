aa89a91366228096baf6d4433e9b1a8c392123d3
Jacob Graham <jakegraham54@gmail.com>
2023-09-12 08:51:29 -0400

    Completed previously uncompleted sections of my website and rewrote some logic rules

--- scripts/index.js
+++ scripts/index.js
@@ -1,16 +1,16 @@
-const MENU_LINKS = document.querySelectorAll('.menu a');
-const WEBSITE_SECTION = document.querySelectorAll('.website_section');
-const CURRENT_SECTION = "current_section_open";
-const CURRENT_LINK_VISITING = "add_active_link_style";
-const PROJECT_DIVS = document.querySelectorAll('.project_div');
-const HOME_PAGE_SENTENCES = ['Computer science student', 'Junior developer', 'Junior designer', 'Junior QA', 'Junior DevOps engineer'];
+const menu_links = document.querySelectorAll('.menu a');
+const website_section = document.querySelectorAll('.website_section');
+const current_section = "current_section_open";
+const current_link_visiting = "add_active_link_style";
+const project_divs = document.querySelectorAll('.project_div');
+const home_page_sentences = ['Computer science student', 'Junior developer', 'Junior designer', 'Junior QA', 'Junior DevOps engineer'];
 
 const typeit_element = new TypeIt("#text", {
     speed: 100,
     loop: true
 });
 
-for (const sentence_string of HOME_PAGE_SENTENCES) {
+for (const sentence_string of home_page_sentences) {
     typeit_element.type(sentence_string).pause(500).delete(sentence_string.length).pause(500);
 }
 typeit_element.go();
@@ -20,21 +20,21 @@ set_section_to_active_when_clicked();
 set_default_link_styling();
 
 function set_default_link_styling(){
-    MENU_LINKS[0].classList.add(CURRENT_LINK_VISITING);
+    menu_links[0].classList.add(current_link_visiting);
 }
 
 function close_menu_when_link_clicked() {
-    MENU_LINKS.forEach(link => link.addEventListener("click", function() {
+    menu_links.forEach(link => link.addEventListener("click", function() {
         document.querySelector(".menu-btn").checked = false;
     }))
 }
 function set_section_to_active_when_clicked() {
-    for (let i = 0; i < MENU_LINKS.length; i++) {
-        MENU_LINKS[i].addEventListener('click', function() {
-            WEBSITE_SECTION.forEach(section => remove_class_from_section(section, CURRENT_SECTION));
-            MENU_LINKS.forEach(link =>  remove_styling_from_active_link(link, CURRENT_LINK_VISITING));
-            add_class_to_section(WEBSITE_SECTION[i], CURRENT_SECTION);
-            add_styling_to_active_link(MENU_LINKS[i], CURRENT_LINK_VISITING);
+    for (let i = 0; i < menu_links.length; i++) {
+        menu_links[i].addEventListener('click', function() {
+            website_section.forEach(section => remove_class_from_section(section, current_section));
+            menu_links.forEach(link =>  remove_styling_from_active_link(link, current_link_visiting));
+            add_class_to_section(website_section[i], current_section);
+            add_styling_to_active_link(menu_links[i], current_link_visiting);
         });
     }
 }
